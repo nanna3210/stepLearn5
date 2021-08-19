91ab99d6db59588da8e4b749a8d02eac991aab3f
nanna3210 <navkishoredas@gmail.com>
2021-08-20 01:44:21 +0530

    lets start new one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,53 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   
-  posts = [
 
-    {
-      title : "A clean  beautiful tree ",
-      imageurl : "assets/tree.jpeg" ,
-      userName  :"nannadas" ,
-      content :  "saw a beautiful tree while hiking the jungle "
-    
-    
-    }, 
-    {
-      title : "A beautifull snowy maountain ", 
-      imageurl : "assets/mountain.jpeg", 
-      userName  : "mountainlover", 
-      content :  "Its A beautifull moment of life when you  get to see a  lovely  maountain near you "
-    
-    
-    },
-    {
-      title : "A beautifull snowy maountain ", 
-      imageurl : "assets/mountain.jpeg", 
-      userName  : "mountainlover", 
-      content :  "Its A beautifull moment of life when you  get to see a  lovely  maountain near you "
-    
-    
-    },
-    {
-      title : "A beautifull snowy maountain ", 
-      imageurl : "assets/mountain.jpeg", 
-      userName  : "mountainlover", 
-      content :  "Its A beautifull moment of life when you  get to see a  lovely  maountain near you "
-    
-    
-    },
-     
-     
-
-    {
-      title : " Mountain biking ", 
-      imageurl :"assets/mountainbiking.jpeg",  
-      userName  : "superbiker", 
-      content :  "thats littera;;y a fun riding bike in mountain "
-    
-    
-    }
-
-  ];
 
   constructor() { 
 

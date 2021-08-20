2f3a0dbd7b1deed189da8ffc01cf276630964ca7
nanna3210 <navkishoredas@gmail.com>
2021-08-20 13:47:57 +0530

    second app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {lorem} from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   
-
-
+  randomText = lorem.sentence()
+  enteredText = '';
   constructor() { 
 
+ 
+      
   }
    
+  onInput(input :string) {
+    this.enteredText=input;
+    
+  }
+  compare(randomText:string, enteredText:string) { 
+    if (!enteredText){ 
+      return "pending"
+    }
+    return (enteredText === randomText) ? 'correct':'incorrect'
+  }
 }
